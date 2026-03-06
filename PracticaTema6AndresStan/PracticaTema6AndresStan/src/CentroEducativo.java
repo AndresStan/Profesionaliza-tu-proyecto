268fa4f8522f8e7ee0f0f4328010701839bbb724
Andres Stan <aastan@iesch.org>
2026-03-06 20:05:50 +0100

    Cambio de atributo ArrayList (lista) a final

--- PracticaTema6AndresStan/PracticaTema6AndresStan/src/CentroEducativo.java
+++ PracticaTema6AndresStan/PracticaTema6AndresStan/src/CentroEducativo.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class CentroEducativo {
 
-    private ArrayList<Trabajador> lista;
+    private final ArrayList<Trabajador> lista;
 
     public CentroEducativo() {
         lista = new ArrayList<>();
