8758c47aec94237064658d59c08f698af718ecea
Andres Stan <aastan@iesch.org>
2026-03-06 20:19:23 +0100

    Eliminacion de codigo innecesario en Trabajador.java

--- PracticaTema6AndresStan/PracticaTema6AndresStan/src/Trabajador.java
+++ PracticaTema6AndresStan/PracticaTema6AndresStan/src/Trabajador.java
@@ -3,20 +3,14 @@ import java.time.Period;
 
 public abstract class Trabajador {
 
-    private String nombre;
-    private String apellidos;
-    private String dni;
-    private LocalDate fechaIncorporacion;
-    private double salarioBase;
+    private final String nombre;
+    private final String apellidos;
+    private final String dni;
+    private final LocalDate fechaIncorporacion;
+    private final double salarioBase;
 
-    public Trabajador(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public Trabajador(String nombre, LocalDate fechaIncorporacion) {
-        this.nombre = nombre;
-        this.fechaIncorporacion = fechaIncorporacion;
-    }
+
 
     public Trabajador(String nombre, String apellidos, String dni, LocalDate fechaIncorporacion, double salarioBase) {
         this.nombre = nombre;
@@ -30,41 +24,16 @@ public abstract class Trabajador {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
 
-    public String getApellidos() {
-        return apellidos;
-    }
 
-    public void setApellidos(String apellidos) {
-        this.apellidos = apellidos;
-    }
 
-    public String getDni() {
-        return dni;
-    }
 
-    public void setDni(String dni) {
-        this.dni = dni;
-    }
-
-    public LocalDate getFechaIncorporacion() {
-        return fechaIncorporacion;
-    }
-
-    public void setFechaIncorporacion(LocalDate fechaIncorporacion) {
-        this.fechaIncorporacion = fechaIncorporacion;
-    }
 
     public double getSalarioBase() {
         return salarioBase;
     }
 
-    public void setSalarioBase(double salarioBase) {
-        this.salarioBase = salarioBase;
-    }
+
 
     public abstract double calcularSalario();
 
